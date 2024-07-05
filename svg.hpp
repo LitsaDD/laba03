a1c2f68bf031757a7262b395bd582a7d76250b1e
LitsaDD <mintyjess57@gmail.com>
2024-07-05 21:01:01 +0300

    code: добавлен svg модуль

---
+++ svg.hpp
@@ -0,0 +1,14 @@
+#ifndef SVG_HPP_INCLUDED
+#define SVG_HPP_INCLUDED
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void svg_begin(double width, double height);
+void svg_end();
+void svg_text(double left, double baseline, string text);
+void svg_rect(double x, double y, double width, double height, string stroke, string fill);
+void show_histogram_svg(const vector<size_t>& bins);
+
+#endif // SVG_HPP_INCLUDED
