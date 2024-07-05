e8fee7dea3b3e44b53c6a6adc4f4edcecf006056
LitsaDD <mintyjess57@gmail.com>
2024-07-05 19:19:06 +0300

    code: добавлено масштабирование

--- main.cpp
+++ main.cpp
@@ -109,13 +109,28 @@ void show_histogram_svg(const vector<size_t>& bins) {
     const auto BLOCK_WIDTH = 10;
 
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
+        }
+    }
+
+    double scaling_factor = 1.;
+    if((max_count * BLOCK_WIDTH) > (IMAGE_WIDTH - TEXT_WIDTH)) {
+       scaling_factor = (IMAGE_WIDTH - TEXT_WIDTH)/(max_count * BLOCK_WIDTH);
+    };
+
     double top = 0;
     for (size_t bin : bins) {
      const double bin_width = BLOCK_WIDTH * bin;
      svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-     svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "blue");
+     svg_rect(TEXT_WIDTH, top, bin_width*scaling_factor, BIN_HEIGHT, "red", "blue");
      top += BIN_HEIGHT;
+
     }
+
     svg_end();
 }
 
