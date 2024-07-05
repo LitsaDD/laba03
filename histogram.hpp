9e7cc5e68fe6c8172bc71d59860ff337cf53472e
LitsaDD <mintyjess57@gmail.com>
2024-07-05 21:00:07 +0300

    code: добавлен модуль для гистограммы

---
+++ histogram.hpp
@@ -0,0 +1,10 @@
+#ifndef HISTOGRAM_HPP_INCLUDED
+#define HISTOGRAM_HPP_INCLUDED
+
+#include <vector>
+
+using namespace std;
+
+void find_minmax(const vector<double> &numbers, double &min, double &max);
+
+#endif // HISTOGRAM_HPP_INCLUDED
